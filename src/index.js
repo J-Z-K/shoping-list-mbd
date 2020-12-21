b272975b2f70229c11575ddb58d520c5f7f5735f
Michal Kadzik <michalkadzik2000@gmail.com>
2020-12-21 17:35:03 +0100

    html structure and basic js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./styles/main.scss";
+
+window.onload = function () {
+  const form = document.querySelector("#form");
+  const list = document.querySelector("#list");
+  console.log(form);
+  form.onsubmit = (e) => {
+    e.preventDefault();
+    const item = document.querySelector("#item").value;
+    const value = parseFloat(document.querySelector("#value").value);
+    const unit = parseInt(
+      document.querySelector('input[name="unit"]:checked').value
+    );
+
+    list.appendChild(generateBlock(item, value, unit));
+    // console.log();
+    return false;
+  };
+};
+
+const generateBlock = (item, value, unit) => {
+  const newDiv = document.createElement("li");
+  const newContent = document.createTextNode(`${item}, ${value}, ${unit}`);
+  newDiv.appendChild(newContent);
+  return newDiv;
+};
